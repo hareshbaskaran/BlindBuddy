227cb754edf73645b617ed14dcf4ce36498c4e6d
haresh <haresh@codez.ai>
2024-04-10 16:55:02 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+const dummy_recieve = "80 percent flower , 20 percent fish";
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
+
+// A screen that allows users to take a picture using a given camera.
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    super.key,
+    required this.camera,
+  });
+
+  final CameraDescription camera;
+
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+  late List<CameraDescription> cameras;
+  Color _bgColor = Colors.white;
+  final FlutterTts flutterTts = FlutterTts();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium,
+    );
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            flutterTts.speak('Hi , you can use the application now , tap to turn on flash light , long press to take a picture , double tap to retake the picture');
+            // todo : If the Future is complete, display the preview.
+            // todo : return a voice message "hi you can use the app now , you can take a picture by long press"
+            return Container(
+              height: MediaQuery
+                  .of(context)
+                  .size
+                  .height - 0.1,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width - 0.05,
+              child: Center(
+                child: GestureDetector(
+                  // on tap - start a flash camera
+                  onTap: () async {
+                    if (_controller.value.isInitialized) {
+                      if (_controller.value.flashMode == FlashMode.off) {
+                        await _controller.setFlashMode(FlashMode.torch);
+                        flutterTts.speak('FlashLight turned on');
+                        setState(() {
+                          _bgColor = Colors.greenAccent;
+                        });
+                      } else {
+                        await _controller.setFlashMode(FlashMode.off);
+                        flutterTts.speak('Flash Light turned off');
+                        setState(() {
+                          _bgColor = Colors.white;
+                        });
+                      }
+                    }
+                  },
+                 ///   onDoubleTap: () {
+                    ///  flutterTts.speak('You can retake the Image Now !');
+                      // dummy implementation
+                      ///  Navigator.of(context).pop(
+                       ///   MaterialPageRoute(
+                      ///      builder: (context) => DisplayFeatureSubScreen(child: Container()
+                      ///    ),
+                     ///     ),
+                  ///      );
+
+                      /// MaterialPageRoute(
+                      /// builder: (context) => DisplayPictureScreen(
+                      /// imagePath: image.path,
+                      /// ),
+                      /// ),
+                      /// );
+                ///  },
+                  // on long press - take a picture
+                  onLongPress: () async {
+                    try {
+                      await _initializeControllerFuture;
+                      final image = await _controller.takePicture();
+                      flutterTts.speak('We have sent the image now , please wait till it is process');
+                      if (!context.mounted) return;
+                      // todo : create api call for this n update in the dummy recieve text
+                      //todo : once picture is taken take picture and post it in backend
+                      // todo : create a await function that during starting tell "wait till we process " and at end " the taken image is + {json data} using flutterTts.speak($text);.
+                      flutterTts.speak('Your picture sent has $dummy_recieve');
+
+
+                       /// await Navigator.of(context).push(
+                        /// MaterialPageRoute(
+                          /// builder: (context) => DisplayPictureScreen(
+                            // Pass the automatically generated path to
+                            // the DisplayPictureScreen widget.
+                            /// imagePath: image.path,
+                          /// ),
+                        /// ),
+                      /// );
+                      ///
+                      // todo : the above implementation is just to show you should also have to create an API that pushes image.path
+                    } catch (e) {
+                      // If an error occurs, log the error to the console.
+                      flutterTts.speak('Sorry , Retake the picture');
+                      print(e);
+                    }
+                  },
+                  child: Container(
+                    height: MediaQuery
+                        .of(context)
+                        .size
+                        .height - 0.1,
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width - 0.05,
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: const Icon(
+                      Icons.settings_voice_outlined,
+                      color: Colors.black,
+                      size: 500,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
